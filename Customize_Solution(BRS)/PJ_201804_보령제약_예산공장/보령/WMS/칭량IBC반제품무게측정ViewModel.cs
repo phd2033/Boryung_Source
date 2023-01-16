68326f05c92ee040ff8b51c1e0e3781bf0408302
21161-NT551XDA\hoyeon <21161@boryung.co.kr>
2023-01-16 10:54:54 +0900

    원료별 칭량 기록시 오더별 구분할수 있도록 오더번호 항목 추가

--- Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/WMS/칭량IBC반제품무게측정ViewModel.cs
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/WMS/칭량IBC반제품무게측정ViewModel.cs
@@ -204,6 +204,11 @@ namespace 보령
                                     _repeater = null;
                                 };
 
+                                #region Campaign Order
+                                OrderList = await CampaignProduction.GetProductionOrderList(_mainWnd.CurrentInstruction.Raw.RECIPEISTGUID, _mainWnd.CurrentOrder.ProductionOrderID);
+                                CanSelectOrder = OrderList.Count > 0 ? true : false;
+                                #endregion
+
                                 // 기본값 setup
                                 btnSaveWeightEnable = false;
 
@@ -602,7 +607,10 @@ namespace 보령
                                 DataSet ds = new DataSet();
                                 DataTable dt = new DataTable("DATA");
                                 ds.Tables.Add(dt);
-                                
+
+                                //2023.01.03 김호연 원료별 칭량을 하면 2개 이상의 배치가 동시에 기록되므로 EBR 확인할때 오더로 구분해야함
+                                dt.Columns.Add(new DataColumn("오더번호"));
+                                //-------------------------------------------------------------------------------------------------------
                                 dt.Columns.Add(new DataColumn("용기번호"));
                                 dt.Columns.Add(new DataColumn("저울번호"));
                                 dt.Columns.Add(new DataColumn("용기중량"));
@@ -615,7 +623,10 @@ namespace 보령
                                 foreach (var item in _IBCList)
                                 {
                                     var row = dt.NewRow();
-                                    
+
+                                    //2023.01.03 김호연 원료별 칭량을 하면 2개 이상의 배치가 동시에 기록되므로 EBR 확인할때 오더로 구분해야함
+                                    row["오더번호"] = item.PoId != null ? item.PoId : "";
+                                    //-------------------------------------------------------------------------------------------------------
                                     row["용기번호"] = item.VesselId != null ? item.VesselId : "";
                                     row["저울번호"] = item.ScaleId != null ? item.ScaleId : "";
                                     row["용기중량"] = item != null ? item.TareWeight.ToString("F" + item.Precision) + " " + _ScaleUom : "";

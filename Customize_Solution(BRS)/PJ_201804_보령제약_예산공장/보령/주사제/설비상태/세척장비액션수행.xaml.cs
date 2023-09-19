3d2c6294d6aeb9b49e27da3f2c1a3a3a1be994df
HDPARK_551XDA\uuser <hdpark@boryung.co.kr>
2023-09-20 08:43:16 +0900

    세척설비액션,캡슐선별반제품 신규화면 생성

---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/주사제/설비상태/세척장비액션수행.xaml.cs
@@ -0,0 +1,58 @@
+﻿using C1.Silverlight.Chart;
+using C1.Silverlight.Chart.Extended;
+using LGCNS.iPharmMES.Common;
+using LGCNS.iPharmMES.Recipe.Common;
+using ShopFloorUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace 보령
+{
+    [Description("SVP 세척 장비 액션 수행")]
+    public partial class 세척장비액션수행 : ShopFloorCustomWindow
+    {
+        세척장비액션수행ViewModel _viewModel;
+
+        public 세척장비액션수행()
+        {
+            InitializeComponent();
+
+            _viewModel = new 세척장비액션수행ViewModel();
+            this.DataContext = _viewModel;
+        }
+        public override string TableTypeName
+        {
+            get { return "TABLE,세척장비액션수행"; }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void c1pkrFromTime_GotFocus(object sender, RoutedEventArgs e)
+        {
+            //var popup = new 세척장비시간입력Popup();
+
+            //popup.resTime = c1pkrFromTime.DateTime.HasValue ? c1pkrFromTime.DateTime.Value : DateTime.Now;
+            //popup.Closed += (s1, e1) =>
+            //{
+            //    if (popup.DialogResult.HasValue && popup.DialogResult.Value)
+            //        c1pkrFromTime.DateTime = Convert.ToDateTime((this.DataContext as 세척장비액션수행ViewModel).FromDt.ToString("yyyy-MM-dd") + popup.resTime.ToString(" HH:mm:ss"));
+            //};
+
+            //popup.Show();
+        }
+    }
+}

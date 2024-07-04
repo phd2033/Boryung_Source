7dd93da0c7fc52534e9e4deec5cf2b38e43165b3
DESKTOP-IEIUIVR\uuser <hdpark@boryung.co.kr>
2024-07-04 13:54:26 +0900

    캡슐개별질량세팅 및 캡슐개별질량확인 신규화면 생성

---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/공정검사/캡슐개별질량확인.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using ShopFloorUI;
+
+namespace 보령
+{
+    public partial class 캡슐개별질량확인 : ShopFloorCustomWindow
+    {
+        public 캡슐개별질량확인()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/공정검사/캡슐개별질량확인ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace 보령
+{
+    public class 캡슐개별질량확인ViewModel
+    {
+
+    }
+}

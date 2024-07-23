4042de75f3f81a81525fb6bcefbbf982752d30aa
DESKTOP-IEIUIVR\uuser <hdpark@boryung.co.kr>
2024-07-23 11:35:05 +0900

    무균공정시트UI추가

---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/주사제/무균공정시트/무균공정시트조회.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace 보령.주사제.무균공정시트
+{
+    public partial class 무균공정시트조회 : Page
+    {
+        public 무균공정시트조회()
+        {
+            InitializeComponent();
+        }
+
+        // 사용자가 이 페이지를 탐색할 때 실행됩니다.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}

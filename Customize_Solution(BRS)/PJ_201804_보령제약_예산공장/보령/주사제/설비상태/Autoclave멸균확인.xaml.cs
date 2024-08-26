18213b98404d9df4688e8333e6fb129844c9adaa
DESKTOP-IEIUIVR\uuser <hdpark@boryung.co.kr>
2024-08-26 11:51:52 +0900

    신규생성

---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/주사제/설비상태/Autoclave멸균확인.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace 보령.주사제.설비상태
+{
+    public partial class Autoclave멸균확인 : Page
+    {
+        public Autoclave멸균확인()
+        {
+            InitializeComponent();
+        }
+
+        // 사용자가 이 페이지를 탐색할 때 실행됩니다.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}

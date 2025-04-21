a34fed9906e32fdffaa170c12d9db589aa717de4
DESKTOP-TU2CSOA\uuser <mmy4637@naver.com>
2025-04-21 12:06:40 +0900

    충전순서 -> 투입번호 이름 변경 및 위치 변경

--- Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/원료투입/소분원료전량투입ViewModel.cs
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/원료투입/소분원료전량투입ViewModel.cs
@@ -314,10 +314,10 @@ namespace 보령
                                 ds.Tables.Add(dt);
                                 dt.Columns.Add(new DataColumn("원료코드"));
                                 dt.Columns.Add(new DataColumn("원료명"));
+                                dt.Columns.Add(new DataColumn("투입번호"));
                                 dt.Columns.Add(new DataColumn("원료시험번호"));
                                 dt.Columns.Add(new DataColumn("바코드"));
-                                dt.Columns.Add(new DataColumn("투입량"));
-                                dt.Columns.Add(new DataColumn("충전순서"));
+                                dt.Columns.Add(new DataColumn("투입량"));                               
                                 dt.Columns.Add(new DataColumn("상태"));
 
                                 var bizRule = new BR_RHR_REG_MaterialSubLot_Dispense_Charging_NEW();
@@ -358,10 +358,10 @@ namespace 보령
                                         row["원료코드"] = item.MTRLID != null ? item.MTRLID : "";
                                         //row["충전순서"] = item.CHGSEQ != null ? item.CHGSEQ : "";
                                         row["원료명"] = item.MTRLNAME != null ? item.MTRLNAME : "";
+                                        row["투입번호"] = item.CHGSEQ.ToString() != null ? item.CHGSEQ.ToString() : "";
                                         row["원료시험번호"] = item.MLOTID != null ? item.MLOTID : "";
                                         row["바코드"] = item.MSUBLOTBCD != null ? item.MSUBLOTBCD : "";
-                                        row["투입량"] = item.CHGQTY != null ? item.CHGQTY + " " + item.UOM : "";
-                                        row["충전순서"] = item.CHGSEQ.ToString() != null ? item.CHGSEQ.ToString() : "";
+                                        row["투입량"] = item.CHGQTY != null ? item.CHGQTY + " " + item.UOM : "";                                     
                                         row["상태"] = item.IS_CAN_CHARGING_CHECKED_NAME != null ? item.IS_CAN_CHARGING_CHECKED_NAME : "";
                                         dt.Rows.Add(row);
                                     }
@@ -464,6 +464,7 @@ namespace 보령
                             ds.Tables.Add(dt);
                             dt.Columns.Add(new DataColumn("원료코드"));
                             dt.Columns.Add(new DataColumn("원료명"));
+                            dt.Columns.Add(new DataColumn("투입번호"));
                             dt.Columns.Add(new DataColumn("원료시험번호"));
                             dt.Columns.Add(new DataColumn("바코드"));
                             dt.Columns.Add(new DataColumn("투입량"));
@@ -472,6 +473,7 @@ namespace 보령
                             var row = dt.NewRow();       
                             row["원료코드"] = "N/A";
                             row["원료명"] = "N/A";
+                            row["투입번호"] = "N/A";
                             row["원료시험번호"] = "N/A";
                             row["바코드"] = "N/A";
                             row["투입량"] = "N/A";

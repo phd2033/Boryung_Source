4cceebe117df4b1c72853c335733276da0c33714
DESKTOP-IEIUIVR\uuser <hdpark@boryung.co.kr>
2025-05-21 13:52:29 +0900

    대전지방식품의약품안전청 GMP 제조소 현장 감시 결과 대응 - 원료의약품라벨 발행제한로직 추가

---
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/라벨발행/라벨발행요청확인.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using ShopFloorUI;
+
+namespace 보령
+{
+    public partial class 라벨발행요청확인 : ShopFloorCustomWindow
+    {
+        public 라벨발행요청확인()
+        {
+            InitializeComponent();
+        }
+        public override string TableTypeName
+        {
+            get { return "TABLE,라벨발행요청확인"; }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = false;
+        }        
+    }
+}

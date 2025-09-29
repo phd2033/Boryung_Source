6767d958f5f3b6fec2a914c1ed730300d9fe22a1
DESKTOP-IEIUIVR\uuser <hdpark@boryung.co.kr>
2025-09-29 15:35:34 +0900

    NA 혹안 N/A일 경우 설비 사용시작, 종료에서 제외

--- Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/주사제/설비상태/멸균종료.cs
+++ Customize_Solution(BRS)/PJ_201804_보령제약_예산공장/보령/주사제/설비상태/멸균종료.cs
@@ -58,6 +58,11 @@ namespace 보령
 
             foreach (var item in inputValues)
             {
+                // 2025.09.29 박희돈 NA 또는 N/A일 경우 사용시작 제외
+                if (!string.IsNullOrWhiteSpace(item.Raw.ACTVAL) && (item.Raw.ACTVAL.ToUpper() == "NA" || item.Raw.ACTVAL.ToUpper() == "N/A"))
+                {
+                    continue;
+                }
 
                 bizRule.INDATAs.Add(new BR_BRS_UPD_EquipmentAction_ShopFloor_PROCEND_STERILE.INDATA()
                 {
